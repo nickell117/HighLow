87ffd19ed08dec90be5886abe22cf98770de7f04
nickell117 <nicholas.karl117@outlook.com>
2020-04-28 09:07:42 -0600

    Initial Commit

---
+++ Ch5HighLow/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Ch5HighLow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            int randomNumber = r.Next(1, 100);
+
+            int guess;
+
+            do
+            {
+                Console.Write("Try and guess the number: ");
+                guess = int.Parse(Console.ReadLine());
+
+                if(guess > randomNumber)
+                {
+                    Console.WriteLine("Thats too High!");
+                }
+                else if (guess < randomNumber)
+                {
+                    Console.WriteLine("Thats too Low!");
+                }
+            } while (guess != randomNumber);
+
+
+            Console.WriteLine("You guessed it!!");
+        }
+    }
+    
+}
---
+++ gitpush.bat
@@ -0,0 +1,8 @@
+
+cd Desktop\MSSA\MSSA CAD\ISTA 220\Code\Ch5HighLow
+git init
+echo "# HighLow" >> README.md
+git add -A
+git commit -m 'Initial Commit'
+git remote add origin https://github.com/nickell117/HighLow.git
+git push -u origin master
